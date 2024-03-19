027c637c4157aa5dbf752767552447dbfce35716
Adil <awanadi515@gmail.com>
2024-03-19 15:54:22 +0500

    eoiwuowq

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -22,38 +22,38 @@ class _SplashViewState extends State<SplashView> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(4289491966),
-              Color(4289491966),
-              Color(4289494015),
-              Color(4289494015),
-              Color(4289494015),
-              Color(4289505535),
-              Color(4288538110),
-              Color(4292214271)
-            ],
-          ),
-        ),
+        // decoration: const BoxDecoration(
+        //   gradient: LinearGradient(
+        //     begin: Alignment.topLeft,
+        //     end: Alignment.bottomRight,
+        //     colors: [
+        //       Color(4289491966),
+        //       Color(4289491966),
+        //       Color(4289494015),
+        //       Color(4289494015),
+        //       Color(4289494015),
+        //       Color(4289505535),
+        //       Color(4288538110),
+        //       Color(4292214271)
+        //     ],
+        //   ),
+        // ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/splashLogo.png',height: 210,width: 210,),
-            const SizedBox(height: 14),
-            const Text(
-              "Find Your Personal Trainer",
-              style: TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 13,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-                height: 5 / 12,
-              ),
-            ),
-            const SizedBox(height: 70),
+            Image.asset('assets/images/splashtrainee.png'),
+            // const SizedBox(height: 14),
+            // const Text(
+            //   "Find Your Personal Trainer",
+            //   style: TextStyle(
+            //     fontFamily: "Poppins",
+            //     fontSize: 13,
+            //     fontWeight: FontWeight.w400,
+            //     color: Colors.white,
+            //     height: 5 / 12,
+            //   ),
+            // ),
+            // const SizedBox(height: 70),
           ],
         ),
       ),
